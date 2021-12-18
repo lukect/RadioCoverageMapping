7459af5907e9bcf59fbe31f83630469feb9d1034
lukect <55721847+lukect@users.noreply.github.com>
2021-12-18 06:39:45 +0000

    Proper requirements.txt setup. Updated matplotlib 3.5.0 -> 3.5.1

--- setup_windows64.ps1
+++ setup_windows64.ps1
@@ -5,4 +5,4 @@ python -m venv venv
 pip install libs\windows64\GDAL-3.3.3-cp310-cp310-win_amd64.whl
 pip install libs\windows64\rasterio-1.2.10-cp310-cp310-win_amd64.whl
 pip install libs\windows64\pyproj-3.2.1-cp310-cp310-win_amd64.whl
-pip install matplotlib
+pip install -r requirements.txt
