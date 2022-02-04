3f8843e6211b248216763390c30f64bfc936db25
lukect <55721847+lukect@users.noreply.github.com>
2022-02-04 06:47:12 +0000

    Add project description.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+Coverage Models for Ad-Hoc 5G Drone Networks
+
+Ad-hoc 5G networks can be used to control drones in mountainous forested regions, which observe the progress of
+fires to plan how to deploy firefighting resources. Since mountainous forested regions do not already have 5G 
+coverage, ad-hoc base station, located on lorries, with their antennas on the end of tethered drones at high altitude 
+can be situated on a road anywhere there is no coverage. The objective of this project is to calculate the coverage 
+area of a deployed set of ad-hoc base stations using propagation path loss models and altitude measurement from 
+Copernicus Maps, so that there is full coverage over the mountainous region to control the fire observation drones.
+"""
+
 import crop_elevation
 import render_elevation_data
 
